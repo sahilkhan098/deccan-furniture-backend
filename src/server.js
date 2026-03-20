5d269b13c0d6634104d26d7e6dd3ad13375cd3b7
sahilkhan098 <itissahilkhan@gmail.com>
2026-03-20 15:42:45 +0530

    Updated server.js

--- src/server.js
+++ src/server.js
@@ -1,70 +1,17 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-
+import app from './app.js';
 import connectDB from './config/db.js';
-
-import orderRoutes from './routes/order.route.js';
-import productRoutes from './routes/product.route.js';
-
-/* =====================================================
-   ENV CONFIG
-===================================================== */
+import dotenv from 'dotenv';
 
 dotenv.config();
-
-/* =====================================================
-   EXPRESS APP
-===================================================== */
-
-const app = express();
-
-/* =====================================================
-   MIDDLEWARE
-===================================================== */
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-/* =====================================================
-   ROUTES
-===================================================== */
-
-// Health Check Route
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
-
-// Product Routes
-app.use('/api/products', productRoutes);
-
-// Order Routes
-app.use('/api/orders', orderRoutes);
-
-/* =====================================================
-   404 HANDLER
-===================================================== */
-
-app.use((req, res) => {
-  res.status(404).json({
-    success: false,
-    message: 'Route not found',
-  });
-});
-
-/* =====================================================
-   SERVER START
-===================================================== */
 
 const PORT = process.env.PORT || 5000;
 
 connectDB()
-  .then(
+  .then(() => {
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
-    }),
-  )
+    });
+  })
   .catch((error) => {
-    throw new Error(error);
+    console.error(error);
   });
